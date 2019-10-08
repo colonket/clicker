d953405e9eb4ac4ff6ec218fafbcfc4177bb70bc
colonEndBracket <figdor@SLATE.localdomain>
2019-10-08 01:02:31 -0500

    playing with own math on asic cost

--- main.js
+++ main.js
@@ -6,17 +6,16 @@ function coinClick(number){
 };
 
 var asics = 0;
-
+var asicCost = 10;
 function buyASIC(){
-    var asicCost = Math.floor(10 * Math.pow(1.1,asics));     //works out the cost of this cursor
     if(coins >= asicCost){                                   //checks that the player can afford the cursor
         asics = asics + 1;                                   //increases number of asics
     	coins = coins - asicCost;                          //removes the coins spent
         document.getElementById('asics').innerHTML = asics;  //updates the number of asics for the user
         document.getElementById('coins').innerHTML = coins;  //updates the number of coins for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,asics));       //works out the cost of the next cursor
-    document.getElementById('asicCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    asicCost = Math.floor(Math.pow(asicCost, 2));       //works out the cost of the next cursor
+    document.getElementById('asicCost').innerHTML = asicCost;  //updates the cursor cost for the user
 };
 
 window.setInterval(function(){
