0b6b7833bcf7645de85e1b86df7af9dd2442b8c9
colonEndBracket <figdor@SLATE.localdomain>
2019-10-07 22:31:24 -0500

    rephrased terms to be crytpocurrency themed

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-var cookies = 0;
+var coins = 0;
 
-function cookieClick(number){
-    cookies = cookies + number;
-    document.getElementById("cookies").innerHTML = cookies;
+function coinClick(number){
+    coins = coins + number;
+    document.getElementById("coins").innerHTML = coins;
 };
 
-var cursors = 0;
+var asics = 0;
 
 function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
-    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
-        cursors = cursors + 1;                                   //increases number of cursors
-    	cookies = cookies - cursorCost;                          //removes the cookies spent
-        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+    var asicCost = Math.floor(10 * Math.pow(1.1,asics));     //works out the cost of this cursor
+    if(coins >= asicCost){                                   //checks that the player can afford the cursor
+        asics = asics + 1;                                   //increases number of asics
+    	coins = coins - asicCost;                          //removes the coins spent
+        document.getElementById('asics').innerHTML = asics;  //updates the number of asics for the user
+        document.getElementById('coins').innerHTML = coins;  //updates the number of coins for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    var nextCost = Math.floor(10 * Math.pow(1.1,asics));       //works out the cost of the next cursor
+    document.getElementById('asicCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
 window.setInterval(function(){
 	
-	cookieClick(cursors);
+	coinClick(asics);
 	
 }, 1000);
